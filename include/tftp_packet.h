dc664c4c8f7b5e1a1e7c7e9562544d840abd7207
Md Shanjid Arefin <shihabkuetcse@gmail.com>
2025-03-13 14:41:13 +0600

    demo_commit

---
+++ include/tftp_packet.h
@@ -0,0 +1,5 @@
+void tftp_packet(void)
+{
+    std::cout<<"Packet new\n";
+    return;
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,11 @@
 #include <bits/stdc++.h>
+#include "tftp_packet.h"
 
 using namespace std;
 
 int main()
 {
     cout<<"TFTP Project v2.\n";
+    tftp_packet();
     return 0;
 }
